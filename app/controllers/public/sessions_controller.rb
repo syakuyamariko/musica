8f4a0bb36b67a39e30701538bbcc02b6756331e3
syakuyamariko <s.mariko1218@gmail.com>
2023-08-14 15:28:46 +0000

    Add relation/withdraw_function

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -31,4 +31,21 @@ class Public::SessionsController < Devise::SessionsController
     #binding.irb
     devise_parameter_sanitizer.permit(:sign_in, keys: [:account_id, :password])
   end
+
+  # 退会しているかを判断するメソッド
+  def reject_user
+      # 【処理内容1】 入力されたemailからアカウントを1件取得、大文字小文字関係なく、メールアドレス自体が合っていたらok
+    @user = user.find_by(email: params[:user][:email])
+    if @user
+      if @user.valid_password?(params[:user][:password]) && (@user.is_deleted == true)
+        flash[:notice] = "退会済みです。再度ご登録をしてご利用ください"
+        redirect_to new_user_registration_path
+      else
+        flash[:notice] = "項目を入力してください"
+      end
+    else
+      flash[:notice] = "該当するユーザーが見つかりません"
+    end
+  end
+
 end
