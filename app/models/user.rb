32b37f3f9489dea151577b2854beb4ed453aaac0
EC2 Default User <s.mariko1218@gmail.com>
2023-09-16 11:17:32 +0000

    FIX withdraw

--- app/models/user.rb
+++ app/models/user.rb
@@ -67,7 +67,7 @@ class User < ApplicationRecord
   GUEST_USER_EMAIL = "guest@example.com"
 
   def self.guest
-    find_or_create_by!(email: GUEST_USER_EMAIL) do |user|
+    find_or_create_by(email: GUEST_USER_EMAIL) do |user|
       user.password = SecureRandom.urlsafe_base64
       user.account_id = "guestuser"
       user.user_name = "ゲスト"
