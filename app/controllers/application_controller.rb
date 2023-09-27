a191b3cf681438722f4be2cc2bf481e907fee41f
EC2 Default User <s.mariko1218@gmail.com>
2023-09-27 06:30:24 +0000

    Add before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
+
   private
 add_flash_types :success, :info, :warning, :danger
 
--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,4 +1,5 @@
 class Public::PostCommentsController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update, :destroy]
 
   def create
     post = Post.find(params[:post_id])
